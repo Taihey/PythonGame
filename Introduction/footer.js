3a3109bfe8a0a221148f80f10161b08b65427953
Taihey <taikan0604@gmail.com>
2023-11-28 02:20:24 +0900

    このゲームの操作方法やプレイ動画です。

---
+++ Introduction/footer.js
@@ -0,0 +1,4 @@
+document.write('        <div><p id="footer">-Rowing Game- 菅野泰平 2025年卒</p></div> ')
+document.write('        <script src="nav.js"></script>')
+document.write('    </body> ')
+document.write('</html> ')
---
+++ Introduction/header.js
@@ -0,0 +1,8 @@
+
+document.write('        <title>Rowing Game</title> ')
+document.write('        <link rel="stylesheet" ')
+document.write('        href="Style.css"> ')
+document.write('    </head> ')
+document.write('    <body> ')
+document.write('        <script src="nav.js"></script>')
+document.write('        <h1>Punch Man</h1> ') 

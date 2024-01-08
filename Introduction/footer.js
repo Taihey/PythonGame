af6fff0658ac358497a6266d5d1e52522a8db523
Taihey <taikan0604@gmail.com>
2024-01-08 11:22:40 +0900

    Punch Manのイントロダクションです。

--- Introduction/footer.js
+++ Introduction/footer.js
@@ -1,4 +1,4 @@
-document.write('        <div><p id="footer">-Rowing Game- 菅野泰平 2025年卒</p></div> ')
+document.write('        <div><p id="footer">-Punch Man- 菅野泰平 2025年卒</p></div> ')
 document.write('        <script src="nav.js"></script>')
 document.write('    </body> ')
 document.write('</html> ')
